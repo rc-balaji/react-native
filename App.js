dbbaa729dadacf836ed5c9783c2369f5c3077bb9
ark-1417 <anieshg682@gmail.com>
2023-08-29 14:39:29 +0530

    BottomTab

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Text, TouchableOpacity, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from './HomeScreen';
 import LoginScreen from './LoginScreen';
 import HomeWorksScreen from './HomeWorksScreen';
 import CollegeWorksScreen from './CollegeWorksScreen';
 import OtherWorksScreen from './OtherWorksScreen';
 import AdminScreen from './AdminScreen';
-import { Text, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   const [completedTasks, setCompletedTasks] = useState([]);
@@ -20,39 +23,101 @@ const App = () => {
     <LoginScreen {...props} setIsLoggedIn={setIsLoggedIn} setUsername={setUsername} />
   );
 
+  const LogoutButton = ({ navigation }) => (
+    <TouchableOpacity
+      onPress={() => setIsLoggedIn(false)}
+      style={{ marginRight: 15 }}
+    >
+      <Text style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>
+        Log Out
+      </Text>
+    </TouchableOpacity>
+  );
+
+  const HomeStack = () => (
+    <Stack.Navigator>
+      
+      <Stack.Screen name="HomeWorks" component={HomeWorksScreen} />
+      <Stack.Screen name="CollegeWorks" component={CollegeWorksScreen} />
+      <Stack.Screen name="OtherWorks" component={OtherWorksScreen} />
+      <Stack.Screen name="Admin" component={AdminScreen} />
+    </Stack.Navigator>
+  );
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {isLoggedIn ? (
-          <>
-            <Stack.Screen
-              name="Home"
-              options={({ navigation }) => ({
-                headerRight: () => (
-                  <TouchableOpacity
-                    onPress={() => setIsLoggedIn(false)}
-                    style={{
-                      marginRight: 15,
-                    }}
-                  >
-                    <Text style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>
-                      Log Out
-                    </Text>
-                  </TouchableOpacity>
-                ),
-              })}
-            >
-              {props => <HomeScreen {...props} setCompletedTasks={setCompletedTasks} username={username} />}
-            </Stack.Screen>
-            <Stack.Screen name="HomeWorks" component={HomeWorksScreen} />
-            <Stack.Screen name="CollegeWorks" component={CollegeWorksScreen} />
-            <Stack.Screen name="OtherWorks" component={OtherWorksScreen} />
-            <Stack.Screen name="Admin" component={AdminScreen} />
-          </>
-        ) : (
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+              headerRight: () => <LogoutButton />,
+            }}
+          />
+          <Tab.Screen
+            name="HomeWorks"
+            component={HomeWorksScreen}
+            options={{
+              tabBarLabel: 'HomeWorks',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="book" color={color} size={size} />
+              ),
+              headerRight: () => <LogoutButton />,
+            }}
+          />
+          <Tab.Screen
+            name="CollegeWorks"
+            component={CollegeWorksScreen}
+            options={{
+              tabBarLabel: 'CollegeWorks',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="school" color={color} size={size} />
+              ),
+              headerRight: () => <LogoutButton />,
+            }}
+          />
+          <Tab.Screen
+            name="OtherWorks"
+            component={OtherWorksScreen}
+            options={{
+              tabBarLabel: 'OtherWorks',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="briefcase" color={color} size={size} />
+              ),
+              headerRight: () => <LogoutButton />,
+            }}
+          />
+          <Tab.Screen
+            name="Admin"
+            component={AdminScreen}
+            options={{
+              tabBarLabel: 'Admin',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="settings" color={color} size={size} />
+              ),
+              headerRight: () => <LogoutButton />,
+            }}
+            listeners={({ navigation }) => ({
+              tabPress: (e) => {
+                const isAdmin = username === "Balaji"; 
+                if (!isAdmin) {
+                  e.preventDefault();
+                  Alert.alert("Access Denied", "Admin Only");
+                }
+              },
+            })}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
           <Stack.Screen name="Login" component={RenderLoginScreen} />
-        )}
-      </Stack.Navigator>
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,90 +1,34 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 
-const HomeScreen = ({ navigation, setIsLoggedIn, username }) => {
-  const navigateToComponent = (componentName) => {
-    navigation.navigate(componentName);
-  };
-
-  const handleAdminPage = () => {
-    const isAdmin = username === "Balaji"; // Checking if the current user is an admin
-
-    if (isAdmin) {
-      navigation.navigate("Admin");
-    } else {
-      Alert.alert("Access Denied", "Admin only");
-    }
-  };
-
+const HomeScreen = ({ username }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={handleAdminPage}>
-          <Text style={styles.adminButton}>Admin</Text>
-        </TouchableOpacity>
-      </View>
-       <Text style={styles.usernameText}>Welcome to your To-do List App</Text>
-       
-      <TouchableOpacity
-        style={styles.componentButton}
-        onPress={() => navigateToComponent('HomeWorks')}
-      >
-        <Text style={styles.componentButtonText}>Home Works</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.componentButton}
-        onPress={() => navigateToComponent('CollegeWorks')}
-      >
-        <Text style={styles.componentButtonText}>College Works</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.componentButton}
-        onPress={() => navigateToComponent('OtherWorks')}
-      >
-        <Text style={styles.componentButtonText}>Other Works</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground
+      source={require('./assets/bgm.png')} 
+      style={styles.container}
+    >
+      
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
+    resizeMode: 'stretch',
+    justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
     marginBottom: 150,
     marginTop: 20,
-    marginLeft:250
+    marginLeft: 250,
   },
   usernameText: {
     fontSize: 18,
     fontWeight: 'bold',
     color: 'black',
-  },
-  adminButton: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: 'blue',
-    marginLeft: 10,
-  },
-  componentButton: {
-    backgroundColor: '#007BFF',
-    borderRadius: 10,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  componentButtonText: {
-    color: '#ffffff',
-    fontSize: 16,
-    fontWeight: 'bold',
   },
 });
 
